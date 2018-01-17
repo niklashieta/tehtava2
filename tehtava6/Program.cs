d879b9cbb83e779ff4f01788128ba12843fe3163
k9279 <k9279@student.jamk.fi>
2018-01-17 16:37:27 +0200

    teht6

---
+++ tehtava6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes1 = new byte[100];
+
+            Random r = new Random();
+            int rInt = r.Next(0, 100);
+            int range = 100;
+            Console.WriteLine(rInt);
+            bool ran = true;
+            Console.WriteLine("Guess a number (0-100)");
+            while (ran)
+            {
+                
+                Console.Write("Guess a number: ");
+                int arvaus = int.Parse(Console.ReadLine());
+                if(arvaus < rInt)
+                {
+                    Console.WriteLine("Number is bigger");
+                }
+                if(arvaus > rInt){
+                    Console.WriteLine("Number is smaller");
+                }
+                if(arvaus == rInt)
+                {
+                    Console.WriteLine("onneksi olkoon olet homo :D");
+                    ran = false;
+                }
+            }
+
+
+
+            
+        }
+    }
+}
+   
