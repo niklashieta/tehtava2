a4ddc11ffd9c04f74e67c9689eb85911a1de4e19
Niklas <Niklas@NIKLAS-PC>
2018-01-18 11:39:38 +0200

    lople

---
+++ tehtava7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, revs = "";
+            Console.WriteLine("Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) //String Reverse
+            {
+               
+                revs += s[i].ToString();
+            }
+            if (revs == s) // Checking whether string is palindrome or not
+            {
+                Console.WriteLine("String is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
